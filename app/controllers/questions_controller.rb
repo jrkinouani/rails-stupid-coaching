e4bb63d0e24f2ee0069aca2e0220b328ea4833e9
jean-rodriguez kinouani <kinouani@gmail.com>
2022-08-15 15:23:30 +0200

    finish exo

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def answer
+    @question = params[:content]
+    @anwser = if @question == 'I am going to work'
+                'Great!'
+              elsif @question.end_with?('?')
+                'Silly question, get dressed and go to work!.'
+              else
+                " I don't care, get dressed and go to work!."
+              end
+  end
+
+  def ask; end
+end
